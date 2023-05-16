1130183dc00da1067c472f94f3bcea9d98187b2e
lukasznowarkiewicz <lukasz.nowarkiewicz@gmail.com>
2023-05-16 09:32:10 +0200

    Builded app for macOS

---
+++ dist/assets/MakeTea-7fd37ccf.js
@@ -0,0 +1,1 @@
+import{d as e,b as a,c as t,u as n,o,h as s}from"./index-9de96461.js";const p=e({__name:"MakeTea",setup(r){return n(),a(s),(c,u)=>(o(),t("h1",null,"Making tea..."))}});export{p as default};
