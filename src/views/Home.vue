b64c019502fc3954b2dfb122c2aa99f83e2ef258
lukasznowarkiewicz <lukasz.nowarkiewicz@gmail.com>
2023-06-10 13:40:51 +0200

    Added missing files

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,14 @@
 <template>
 <!-- :nazwa deklaruje properties przekazywane z rodzica na dziecko (pod VUE)-->
+  <button style="margin-top: -10em; margin-left: 70%; position: absolute"><div class="icon">
+    <div class="cogs">
+      <div class="cog"></div>
+      <div class="cog"></div>
+      <div class="cog"></div>
+      <div class="cog"></div>
+    </div>
+    <div class="wheel"></div>
+  </div></button>
   <TeaPreview v-for="tea in teaData" :key="tea.id" :tea="tea" @click="redirectToTeaDetails(tea.slug)" />
 
 </template>
@@ -21,3 +30,57 @@ const teaData = ref(teaJson);
 </script>
 
 <!-- component lifecycle VUE - poczytac  -->
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+.icon {
+  position: relative;
+  height: 80px;
+  width: 80px;
+}
+
+.wheel {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  width: 16px;
+  height: 16px;
+  background: rgb(0, 0, 0);
+  border-radius: 50%;
+  border: solid 12px brown;
+}
+
+.cog {
+  position: absolute;
+  width: 14px;
+  height: 60px;
+  border-radius: 3px;
+  background: brown;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto;
+}
+
+.cog:nth-child(2) {
+  transform: rotate(45deg);
+}
+
+.cog:nth-child(3) {
+  transform: rotate(90deg)
+}
+
+.cog:nth-child(4) {
+  transform: rotate(135deg)
+}
+
+
+</style>
