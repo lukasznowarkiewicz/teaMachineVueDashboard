ea499054d33d030a3e646ab4aacd2e62f08e89e4
lukasznowarkiewicz <lukasz.nowarkiewicz@gmail.com>
2023-06-10 13:44:33 +0200

    Just for test pushing builded armv7 app

---
+++ target/armv7-unknown-linux-gnueabihf/debug/deps/tea_machine_app-46e4ff60dc6a2d9a.d
@@ -0,0 +1,5 @@
+/home/repo/tea-machine-app/target/armv7-unknown-linux-gnueabihf/debug/deps/tea_machine_app-46e4ff60dc6a2d9a: src/main.rs
+
+/home/repo/tea-machine-app/target/armv7-unknown-linux-gnueabihf/debug/deps/tea_machine_app-46e4ff60dc6a2d9a.d: src/main.rs
+
+src/main.rs:
---
+++ target/armv7-unknown-linux-gnueabihf/debug/tea-machine-app.d
@@ -0,0 +1,1 @@
+/home/repo/tea-machine-app/target/armv7-unknown-linux-gnueabihf/debug/tea-machine-app: /home/repo/tea-machine-app/src/main.rs
